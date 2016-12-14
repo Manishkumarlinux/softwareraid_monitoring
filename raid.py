7772805e83d043ac731aed4a60bc1b7c08cfb8bf
Manishkumarlinux <manishkumarlinux@gmail.com>
2016-12-14 11:53:55 +0530

    Create raid.py

---
+++ raid.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import os
+import sys
+from pymdstat import MdStat
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEImage import MIMEImage
+
+fp = open('/opt/raid.txt', 'r')
+msg = MIMEText(fp.read())
+fp.close()
+
+#Change Email ID accordingly
+me = 'email@domain.com'
+you = 'email@domain.com'
+msg['Subject'] = 'RAID Problem'
+msg['From'] = me
+msg['To'] = you
+s = smtplib.SMTP("localhost",25)
+s.ehlo()
+raidcmd = "cat /proc/mdstat" + ">" + "/opt/raid.txt"
+
+mds = MdStat()
+raid = ['md0', 'md1', 'md2', 'md125']
+
+for i in raid:
+    if mds.used(i) == 2:
+        print "raid not ok"
+
+    else:
+        os.system(raidcmd)
+        s.sendmail(me, [you], msg.as_string())
+        print 'done!'
+        s.quit()   
